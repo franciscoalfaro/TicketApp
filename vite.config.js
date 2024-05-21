cef6c6c45dd1d79013d24d48f40e78f28ef78c54
franciscoalfar@gmail.com <*Fran2811*>
2024-05-21 18:17:48 -0400

    server

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
   proxy:{
     '/socket.io':{
-      target:'https://franalfaro.ddns.net/serverTicket',
+      target:'http://localhost:3000',
       ws:true
     }
   }
