9d985aadfe5a43eeaf58c4cf919ea067707d6b85
franciscoalfar@gmail.com <*Fran2811*>
2024-05-24 18:33:34 -0400

    voitr

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TicketApp/', // Define el subdirectorio base
   server: {
     proxy: {
       '/socket.io': {
         target: 'http://localhost:3000/',
-        ws: true
+        ws: true,
+        changeOrigin: true,
+        secure: false
       }
     }
   }
