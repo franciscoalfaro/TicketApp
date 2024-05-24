7a36c161aa8f58efe0c1f5220ad983c8653b8e4c
franciscoalfar@gmail.com <*Fran2811*>
2024-05-24 17:50:02 -0400

    mejora

--- src/pages/RouterPage.jsx
+++ src/pages/RouterPage.jsx
@@ -1,14 +1,12 @@
 import React, { useContext } from 'react';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 import { Layout, Menu, theme } from 'antd';
-import { BrowserRouter as Router, Link, Route, Routes, Outlet, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes, Navigate } from 'react-router-dom';
 import { Ingresar } from './Ingresar';
 import { Cola } from './Cola';
 import { CrearTicket } from './CrearTicket';
 import { Escritorio } from './Escritorio';
 import { UiContext } from '../context/UiContext';
-
-
 
 const { Content, Footer, Sider } = Layout;
 
@@ -17,12 +15,12 @@ export const RouterPage = () => {
         token: { colorBgContainer },
     } = theme.useToken();
 
-    const {ocultarMenu} = useContext(UiContext)
+    const { ocultarMenu } = useContext(UiContext);
 
     return (
         <Router>
             <Layout style={{ height: '100vh' }}>
-                <Sider collapsedWidth='0'  breakpoint="md" hidden={ocultarMenu}>
+                <Sider collapsedWidth='0' breakpoint="md" hidden={ocultarMenu}>
                     <div className="demo-logo-vertical"></div>
                     <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                         <Menu.Item key="1" icon={<UserOutlined />}>
@@ -39,7 +37,6 @@ export const RouterPage = () => {
                 <Layout>
                     <Content style={{ margin: '24px 16px 0' }}>
                         <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
-                            {/* The components specified in the Route elements will be rendered based on the current route */}
                             <Routes>
                                 <Route path="/ingresar" element={<Ingresar />} />
                                 <Route path="/cola" element={<Cola />} />
@@ -47,7 +44,7 @@ export const RouterPage = () => {
                                 <Route path="/escritorio" element={<Escritorio />} />
 
                                 {/* Redirect to "/ingresar" if no route matches */}
-                                <Route path="/" element={<Navigate to="/ingresar" />} />
+                                <Route path="*" element={<Navigate to="/ingresar" replace />} />
                             </Routes>
                         </div>
                     </Content>
